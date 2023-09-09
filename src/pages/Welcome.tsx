23f612b73320f87c5a4652398d6dfb0117091d2f
EugenyTR <takeseoeasy@gmail.com>
2023-09-09 21:45:59 +0700

    get query params

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,17 @@
+import { useSearchParams } from "react-router-dom"
+
 export const Welcome = () => {
+    let [searchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+
     return (
-        <h1>Welcome</h1>
+        <div>
+            <h1>
+                id = {id}
+            </h1>
+        
+            <h2>Welcome</h2>
+        </div>
     )
 }
